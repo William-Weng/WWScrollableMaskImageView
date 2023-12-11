fec6eff611eb1cd52e77e67870a4a718fd4c7ef5
William.Weng <William.Weng@iit.com.hk>
2023-12-11 17:27:49 +0800

    - v1.0.0

---
+++ Example/Example/ViewController/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by William.Weng on 2023/12/11.
+//  ~/Library/Caches/org.swift.swiftpm/
+
+import UIKit
+import WWScrollableMaskImageView
+
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var maskView: WWScrollableMaskImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let barImage = UIImage(named: "Bar")
+        let barNinePngImage = barImage?.resizableImage(withCapInsets: UIEdgeInsets(top: 30, left: 0, bottom: 30, right: 0), resizingMode: .stretch)
+        
+        maskView.setting(barImage: barNinePngImage, contentMode: .scaleToFill, color: .clear)
+    }
+}
+
